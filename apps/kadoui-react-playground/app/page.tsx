6028913dfd964c242fd8adcf8c14e0092350a444
FarzadVav <farzad.vav.work@gmail.com>
2025-08-04 14:46:55 +0330

    fix kadoui-react-playground scripts

--- apps/kadoui-react-playground/app/page.tsx
+++ apps/kadoui-react-playground/app/page.tsx
@@ -1,7 +1,13 @@
+import { AccessNavigation } from "kadoui-react"
+
 function Page() {
   return (
     <div className="container my-3">
-      <button className="btn-fill">Click me</button>
+      <AccessNavigation direction="x" className="f-align gap-3">
+        <button className="btn-fill">Click me 1</button>
+        <button className="btn-fill">Click me 2</button>
+        <button className="btn-fill">Click me 3</button>
+      </AccessNavigation>
     </div>
   )
 }
